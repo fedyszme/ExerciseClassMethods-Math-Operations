d8cc5ecd3fba874eff0661f85087377721eaa93f
fedyszme <36646062+fedyszme@users.noreply.github.com>
2018-04-19 16:48:10 -0400

    Add files

---
+++ ExerciseClassMethods/MathOperations.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseClassMethods
+{
+    public class MathOperations
+    {
+      public static int Square(int i)
+        {
+            int input = i;
+            return input * input;
+            
+        }
+
+       public static int MultbyTen(int i)
+        {
+           int input = i;
+            return input * 10;
+        }
+
+       public static int AddbyTen(int i)
+        {
+            int input = i;
+            return input + 10;
+        }
+
+        
+    }
+
+    
+}
---
+++ ExerciseClassMethods/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseClassMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please pick an integer to perform math operations on");
+            int pickNum = Convert.ToInt32(Console.ReadLine());
+
+            int result1 = MathOperations.AddbyTen(pickNum);
+            Console.WriteLine("Your number " + pickNum + " plus 10 eqauls " + result1);
+
+            int result2 = MathOperations.MultbyTen(pickNum);
+            Console.WriteLine("Your number " + pickNum + " multiplied by 10 eqauls " + result2);
+
+            int result3 = MathOperations.Square(pickNum);
+            Console.WriteLine("Your number " + pickNum + " squared equals" + result3);
+
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
